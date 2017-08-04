5901dc1017dd5cbc0d7fa28f80cbda1d0f3f8e10
haugan <hauganmarius@gmail.com>
2017-08-04 15:50:29 +0200

    Initial controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Culinar.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Culinar.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repo;
+        
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET product/list
+        public ViewResult List() => View(repo.Products);
+    }
+}

081980cbae01b5390dcd49855e568ce5d394dbce
haugan <hauganmarius@gmail.com>
2017-08-04 15:21:17 +0200

    Initial (mock) Product repository

---
+++ Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Culinar.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product {Name = "Olive oil", Price = 20},
+            new Product {Name = "Black rice", Price = 30},
+            new Product {Name = "Chopped lamb", Price = 75}
+        }
+    }
+}
